fc68b77282cc232817eb0ea8174acec2aa5ec656
Vinayak Chaturvedi <vinayak.chaturvedi96@gmail.com>
2020-09-16 21:37:10 +0530

    Code Refactor

--- 07.c
+++ 07.c
@@ -3,10 +3,13 @@
 #include <fcntl.h>
 
 int main(){
+
+    //----------------------------------------Opening the main file--------------------------------------
     int fdMainFile = open("MainFileQues7.txt", O_RDONLY);
     const int max = 100000;
     if(fdMainFile == -1){
-        char message[] = "File Doesn't Exist with name as \"MainFileQues7.txt\" hence creating one\n";
+    //----------------------------------------Main file doesn't exists hence creating one----------------------
+        char message[] = "File Doesn't Exists with name as \"MainFileQues7.txt\" hence creating one\n";
         write(2, message, sizeof(message)-1);
         //perror("Error");
         fdMainFile = open("MainFileQues7.txt", O_CREAT|O_WRONLY|O_TRUNC, 0644);
@@ -14,10 +17,15 @@ int main(){
         write(fdMainFile, content, sizeof(content) - 1);
         close(fdMainFile);
     }
+
+    //----------------------------------------Opening the main file in readOnly mode--------------------------------------
     fdMainFile = open("MainFileQues7.txt", O_RDONLY);
 
+    //----------------------------------------Reading the content of main file------------------------------------
     char content[max];
     read(fdMainFile, content, sizeof(content));
+
+    //----------------------------------------Creating copied file and writing to it--------------------------------------
     int fdCopiedFile = open("CopiedFileQues7.txt", O_CREAT|O_WRONLY|O_TRUNC, 0644);
     int i = 0;
     while(content[i] != '\0'){
@@ -25,6 +33,7 @@ int main(){
         write(fdCopiedFile, copiedContent, sizeof(copiedContent));
     }
 
+    //----------------------------------------Closing both FD--------------------------------------
     close(fdMainFile);
     close(fdCopiedFile);
     return 0;
