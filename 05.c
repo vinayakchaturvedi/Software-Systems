6fca64e1a2313fc5bd18972ed94d090eb8cb97d4
Vinayak Chaturvedi <vinayak.chaturvedi96@gmail.com>
2020-09-16 20:48:22 +0530

    Comment Refactor

--- 05.c
+++ 05.c
@@ -21,19 +21,23 @@ int main() {
 /*
 vinayak@vinayak-Lenovo-G50-70:~/Desktop/Study_Material/1stSem/SS/IJHandsOn$ cc 05.c
 vinayak@vinayak-Lenovo-G50-70:~/Desktop/Study_Material/1stSem/SS/IJHandsOn$ ./a.out &
-[1] 51041
-vinayak@vinayak-Lenovo-G50-70:~/Desktop/Study_Material/1stSem/SS/IJHandsOn$ ll /proc/51041/fd/
+[1] 8670
+vinayak@vinayak-Lenovo-G50-70:~/Desktop/Study_Material/1stSem/SS/IJHandsOn$ ll /proc/8670/fd/
 total 0
-dr-x------ 2 vinayak vinayak  0 Sep 15 23:11 ./
-dr-xr-xr-x 9 vinayak vinayak  0 Sep 15 23:11 ../
-lrwx------ 1 vinayak vinayak 64 Sep 15 23:11 0 -> /dev/pts/2
-lrwx------ 1 vinayak vinayak 64 Sep 15 23:11 1 -> /dev/pts/2
-lrwx------ 1 vinayak vinayak 64 Sep 15 23:11 2 -> /dev/pts/2
-lr-x------ 1 vinayak vinayak 64 Sep 15 23:11 3 -> /home/vinayak/Desktop/Study_Material/1stSem/SS/IJHandsOn/File5thQues0.txt
-lr-x------ 1 vinayak vinayak 64 Sep 15 23:11 4 -> /home/vinayak/Desktop/Study_Material/1stSem/SS/IJHandsOn/File5thQues1.txt
-lr-x------ 1 vinayak vinayak 64 Sep 15 23:11 5 -> /home/vinayak/Desktop/Study_Material/1stSem/SS/IJHandsOn/File5thQues2.txt
-lr-x------ 1 vinayak vinayak 64 Sep 15 23:11 6 -> /home/vinayak/Desktop/Study_Material/1stSem/SS/IJHandsOn/File5thQues3.txt
-lr-x------ 1 vinayak vinayak 64 Sep 15 23:11 7 -> /home/vinayak/Desktop/Study_Material/1stSem/SS/IJHandsOn/File5thQues4.txt
-vinayak@vinayak-Lenovo-G50-70:~/Desktop/Study_Material/1stSem/SS/IJHandsOn$
-
+dr-x------ 2 vinayak vinayak  0 Sep 16 20:47 ./
+dr-xr-xr-x 9 vinayak vinayak  0 Sep 16 20:47 ../
+lrwx------ 1 vinayak vinayak 64 Sep 16 20:47 0 -> /dev/pts/1
+lrwx------ 1 vinayak vinayak 64 Sep 16 20:47 1 -> /dev/pts/1
+lrwx------ 1 vinayak vinayak 64 Sep 16 20:47 2 -> /dev/pts/1
+lr-x------ 1 vinayak vinayak 64 Sep 16 20:47 3 -> /home/vinayak/Desktop/Study_Material/1stSem/SS/IJHandsOn/File5thQues0.txt
+lr-x------ 1 vinayak vinayak 64 Sep 16 20:47 4 -> /home/vinayak/Desktop/Study_Material/1stSem/SS/IJHandsOn/File5thQues1.txt
+lr-x------ 1 vinayak vinayak 64 Sep 16 20:47 5 -> /home/vinayak/Desktop/Study_Material/1stSem/SS/IJHandsOn/File5thQues2.txt
+lr-x------ 1 vinayak vinayak 64 Sep 16 20:47 6 -> /home/vinayak/Desktop/Study_Material/1stSem/SS/IJHandsOn/File5thQues3.txt
+lr-x------ 1 vinayak vinayak 64 Sep 16 20:47 7 -> /home/vinayak/Desktop/Study_Material/1stSem/SS/IJHandsOn/File5thQues4.txt
+vinayak@vinayak-Lenovo-G50-70:~/Desktop/Study_Material/1stSem/SS/IJHandsOn$ kill -9 8670
+vinayak@vinayak-Lenovo-G50-70:~/Desktop/Study_Material/1stSem/SS/IJHandsOn$ ps
+    PID TTY          TIME CMD
+   8522 pts/1    00:00:00 bash
+   8677 pts/1    00:00:00 ps
+[1]+  Killed                  ./a.out
 */
