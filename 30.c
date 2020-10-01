07da22ce3a5f1f462ae8e06c98e9d206aa909870
Vinayak Chaturvedi <vinayak.chaturvedi96@gmail.com>
2020-10-02 01:13:44 +0530

    Adding 30th question

--- 30.c
+++ 30.c
@@ -1,19 +1,36 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
-#include <sys/types.h>
-true#include <sys/stat.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include <time.h>
 
 int main(){
+
+    int hour, min, sec;
+    printf("Enter the time at which you want to script to be executed\nHour: ");
+    scanf("%d", &hour);
+    printf("Minute: ");
+    scanf("%d", &min);
 
     if(!fork()){            //child
         printf("Child process id: %d\n", getpid());
         setsid();
         chdir("/");
         umask(0);
+
+        time_t rawtime;
+        struct tm* timeinfo;
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
         while(1){
-            sleep(5);
-            printf("Daemon process is running...\n");
+            if(timeinfo->tm_hour == hour && timeinfo->tm_min == min){
+                execlp("ls", "ls", "-lrt", "/home/", NULL);
+                printf("Done");
+                break;
+            }
+            time(&rawtime);
+            timeinfo = localtime(&rawtime);
         }
     }
     else{
